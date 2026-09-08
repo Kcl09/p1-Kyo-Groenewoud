6ab1b433f10904eaddc0e3b8dda03a0a0c543e47
Kcl09 <chilamgroenewoud@gmail.com>
2026-09-08 15:45:49 +0200

    verder gegaan met week taak

--- Week 1 hello world/sketch.js
+++ Week 1 hello world/sketch.js
@@ -1,9 +1,104 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
 function draw() {
   background(220);
 
-circle (200,200,100);
+  //opdracht 1
+  fill("black")
+
+  noStroke()
+textSize(12);
+text("1 Kyo Groenewoud",50,50);
+
+//opdracht 2
+text("2",50,100);
+
+noStroke();
+fill(255,0,0);
+rect(60,100,150,40);
+fill(255);
+rect(60,140,150,40);
+fill(0,0,255);
+rect(60,180,150,40);
+
+fill("white");
+
+//opdracht 3
+
+stroke("black");
+square(60,250,150);
+
+
+noStroke()
+fill("black");
+square(160,350,50);
+square(110,300,50);
+square(60,350,50);
+square(60,250,50);
+square(160,250,50);
+
+noStroke()
+text("3",50,250);
+
+//opdracht 4
+noStroke(1)
+text("4",50,450);
+
+stroke(1)
+fill(220);
+square(60,450,50);
+fill
+triangle(60,450,110,450,85,410)
+
+//opdracht 5
+noStroke();
+fill("black");
+text("5",290,50);
+fill(150);
+rect(300,50,50,180);
+rect(313,230,25,40);
+
+fill(255,0,0);
+circle(325,75,40);
+
+fill(255,190,0);
+circle(325,130,40);
+
+fill(10,255,0);
+circle(325,185,40);
+
+//opdracht 6
+fill("black");
+text('6',290,300);
+strokeWeight(5);
+stroke(1);
+fill("white");
+square(300,300,100,30);
+
+fill("black")
+noStroke()
+circle(325,325,25)
+circle(350,350,25)
+circle(375,375,25)
+
+//opdracht 7
+text("7",400,50)
+
+fill('red')
+rect(420,40,60,10)
+rect(410,50,100,10)
+
+fill('black')
+rect(470,60,10,20)
+rect(480,80,10,10)
+rect(470,90,40,10)
+
+fill(220,170,145)
+rect(480,70,30,10)
+rect(490,80,30,10)
+rect(470,80,10,10)
+rect(420,60,50,50)
+rect(470,100,30,10)
 }
