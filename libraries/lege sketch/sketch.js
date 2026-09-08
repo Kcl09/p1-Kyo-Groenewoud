77a6c3b71943153e7057176a7ab9611096046de2
Kcl09 <chilamgroenewoud@gmail.com>
2026-09-08 12:43:16 +0200

    mondriaan gemaakt in sketch.js

--- libraries/lege sketch/sketch.js
+++ libraries/lege sketch/sketch.js
@@ -1,7 +1,22 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+
 }
 
 function draw() {
   background(220);
+noStroke();
+fill(150);
+rect(50,50,50,180);
+rect(63,230,25,40);
+
+fill(255,0,0);
+circle(75,75,40);
+
+fill(255,190,0);
+circle(75,130,40);
+
+fill(0,255,0);
+circle(75,185,40);
 }
+
