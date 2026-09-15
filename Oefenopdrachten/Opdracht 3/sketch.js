3e1ce89b7579372d17047c05465c501646258829
Kcl09 <chilamgroenewoud@gmail.com>
2026-09-15 11:42:31 +0200

    oefen opdracht 3 klaar

--- Oefenopdrachten/Opdracht 3/sketch.js
+++ Oefenopdrachten/Opdracht 3/sketch.js
@@ -1,7 +1,23 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 200);
 }
 
+
 function draw() {
-  background(220);
+background(220);
+
+noLoop();
+let score= random(100);;
+
+
+if (score <= 50){
+  text("onvoldoende",10,10);
+} else if (score <= 69){
+  text("voldoende",10,10);
+} else if (score <=89){
+  text("goed",10,10)
+}else if(score >=90) {
+  text("uitstekend",10,10)
+}
+
 }
